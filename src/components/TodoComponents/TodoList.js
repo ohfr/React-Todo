322fc0f83fa0fc35fa70a4b0a2a8a4127212b0a2
Daniel Martin <danielmartin1205@gmail.com>
2019-10-01 22:13:08 -0400

    mvp goal

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+import './Todo.css';
+
+export default class TodoList extends React.Component {
+    render() {
+        return (
+            <div>
+                <TodoForm addItem={this.props.addItem} deleteItem={this.props.deleteItem} />
+                {this.props.items.map((cur, i) => {
+                    return <Todo toggleItem={this.props.toggleItem} item={cur.task} id={cur.id} key={i} completed={cur.completed} />
+                })}
+            </div>
+        )
+    }
+}
